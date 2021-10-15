8b55e5f34b909d712c49a414dc4680679f345e01
illrock <illroman@yandex.ru>
2021-10-15 19:35:17 +0300

    Initial commit

---
+++ app/src/main/java/my/illrock/fcmapchallenge/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package my.illrock.fcmapchallenge.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import my.illrock.fcmapchallenge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
